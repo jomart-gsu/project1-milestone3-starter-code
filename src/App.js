4c2ea2325cb9d1f1b953e27dbb57ba8f77487f09
jomart-gsu <jmartin191@gsu.edu>
2021-10-18 21:43:06 -0400

    removed extra HTML file, added command to parse JSON args

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState, useRef} from 'react';
+import { useState, useRef } from 'react';
 
 
 function App() {
+  // fetches JSON data passed in by flask.render_template and loaded
+  // in public/index.html in the script with id "data"
+  const args = JSON.parse(document.getElementById("data").text);
+
   // TODO: Implement your main page as a React component.
 }
 
